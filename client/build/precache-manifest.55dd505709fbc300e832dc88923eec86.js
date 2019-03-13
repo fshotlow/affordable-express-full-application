e9ff79c246c94e1beb72470e1b6cc60033e4fb18
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-03-13 17:47:05 -0500

    update gitignore

---
+++ client/build/precache-manifest.55dd505709fbc300e832dc88923eec86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "810da0f4d5545829ea62",
+    "url": "/static/css/main.e638e73b.chunk.css"
+  },
+  {
+    "revision": "810da0f4d5545829ea62",
+    "url": "/static/js/main.db028aaa.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2aac0bfdee3ba01f9317",
+    "url": "/static/css/2.f58cfc70.chunk.css"
+  },
+  {
+    "revision": "2aac0bfdee3ba01f9317",
+    "url": "/static/js/2.d3856026.chunk.js"
+  },
+  {
+    "revision": "26ad599874a8ef5988bdfb0a200efd45",
+    "url": "/index.html"
+  }
+];
